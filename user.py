4083f219cf2ebb8b40354aca61e398f66558ca6a
koyoo  maxwel <koyoomaxwel@gmail.com>
2018-08-25 12:02:44 +0300

    find user by number test is WORKING

--- user.py
+++ user.py
@@ -23,7 +23,32 @@ class User:
             '''
 
             User.user_list.append(self)
-            
+
+        
+        def delete_user(self):
+            '''
+            delete _user method deletes a saved user from the user_list
+            '''
+        
+            User.user_list.remove(self)
+
+        @classmethod
+        def find_by_number(cls,number):
+            '''
+            Method that takes in a number and returns a user that matches that number.
+
+            Args:
+            number: Phone number to search for
+            Returns :
+            user of person that matches the number.
+            '''
+
+            for user in cls.user_list:
+                 if user.phone_number == number:
+                  return user
+
+
+    
 
 
 
--- user_test.py
+++ user_test.py
@@ -48,11 +48,35 @@ class TestUser(unittest.TestCase):
         test_save_multiple_user to check if we can save multiple user objects to our user_list
         '''
         self.new_user.save_user()
-        test_user = User("a","b","c","d") # new user
+        test_user = User("koyoo","maxwel","0204405922","koyoomaxwel@gmail.com") # new user
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
+    
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we we can remove user from our user list
+        '''
+        self.new_user.save_user()
+        test_user = User("koyoo","maxwel","0204405922","koyoomaxwel@gmail.com") # new user
+        test_user.save_user()
 
+        self.new_user.delete_user() # deleting a user object
+        self.assertEqual(len(User.user_list),1)
+    
+    def test_find_user_by_number(self):
+        '''
+        test to check if we can find a user by phone number and display information
+        '''
 
+        self.new_user.save_user()
+        test_user = User("koyoo","maxwel","0204405922","koyoomaxwel@gmail.com") # new user
+        test_user.save_user()
+
+        found_user = User.find_by_number("0204405922")
+
+        self.assertEqual(found_user.email,test_user.email)
+
+    
 
 
 if __name__ == '__main__':
